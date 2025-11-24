56b408f5049d597b0dccf167f8f067b5c4af5cd0
Kazi Anik Islam <anikkazi14@gmail.com>
2025-11-24 23:27:03 +0100

    B04

--- src/adap-b04/files/File.ts
+++ src/adap-b04/files/File.ts
@@ -3,34 +3,32 @@ import { Directory } from "./Directory";
 import { MethodFailedException } from "../common/MethodFailedException";
 
 enum FileState {
-    OPEN,
-    CLOSED,
-    DELETED        
-};
+  OPEN,
+  CLOSED,
+  DELETED,
+}
 
 export class File extends Node {
+  protected state: FileState = FileState.CLOSED;
 
-    protected state: FileState = FileState.CLOSED;
+  constructor(baseName: string, parent: Directory) {
+    super(baseName, parent);
+  }
 
-    constructor(baseName: string, parent: Directory) {
-        super(baseName, parent);
-    }
+  public open(): void {
+    // do something
+  }
 
-    public open(): void {
-        // do something
-    }
+  public read(noBytes: number): Int8Array {
+    // read something
+    return new Int8Array();
+  }
 
-    public read(noBytes: number): Int8Array {
-        // read something
-        return new Int8Array();
-    }
+  public close(): void {
+    // do something
+  }
 
-    public close(): void {
-        // do something
-    }
-
-    protected doGetFileState(): FileState {
-        return this.state;
-    }
-
+  protected doGetFileState(): FileState {
+    return this.state;
+  }
 }
--- src/adap-b04/names/AbstractName.ts
+++ src/adap-b04/names/AbstractName.ts
@@ -1,57 +1,82 @@
-import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { MethodFailedException } from "../common/MethodFailedException";
+
 import { Name } from "./Name";
+import { DEFAULT_DELIMITER } from "../common/Printable";
 
 export abstract class AbstractName implements Name {
+  public abstract clone(): Name;
+  public abstract getNoComponents(): number;
+  public abstract getComponent(i: number): string;
+  public abstract setComponent(i: number, c: string): void;
+  public abstract insert(i: number, c: string): void;
+  public abstract append(c: string): void;
+  public abstract remove(i: number): void;
+  public abstract getDelimiterCharacter(): string;
 
-    protected delimiter: string = DEFAULT_DELIMITER;
-
-    constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+  public asString(delimiter: string = this.getDelimiterCharacter()): string {
+    IllegalArgumentException.assert(
+      this.isValidDelimiter(delimiter),
+      "Invalid delimiter"
+    );
+    let components: string[] = [];
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      components.push(this.getComponent(i));
     }
+    return components.join(delimiter);
+  }
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
+  public toString(): string {
+    return this.asDataString();
+  }
+
+  public asDataString(): string {
+    return this.asString(DEFAULT_DELIMITER);
+  }
+
+  public isEqual(other: Name): boolean {
+    IllegalArgumentException.assert(
+      other !== null && other !== undefined,
+      "Other name cannot be null or undefined"
+    );
+    if (this.getNoComponents() !== other.getNoComponents()) {
+      return false;
     }
-
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+    for (let i = 0; i < this.getNoComponents(); i++) {
+      if (this.getComponent(i) !== other.getComponent(i)) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    public toString(): string {
-        return this.asDataString();
+  public getHashCode(): number {
+    let hashCode: number = 0;
+    const s: string = this.asDataString();
+    for (let i: number = 0; i < s.length; i++) {
+      let c: number = s.charCodeAt(i);
+      hashCode = (hashCode << 5) - hashCode + c;
+      hashCode |= 0;
     }
+    return hashCode;
+  }
 
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+  public isEmpty(): boolean {
+    return this.getNoComponents() === 0;
+  }
+
+  public concat(other: Name): void {
+    IllegalArgumentException.assert(
+      other !== null && other !== undefined,
+      "Other name cannot be null or undefined"
+    );
+    for (let i = 0; i < other.getNoComponents(); i++) {
+      this.append(other.getComponent(i));
     }
+  }
 
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    abstract getNoComponents(): number;
-
-    abstract getComponent(i: number): string;
-    abstract setComponent(i: number, c: string): void;
-
-    abstract insert(i: number, c: string): void;
-    abstract append(c: string): void;
-    abstract remove(i: number): void;
-
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
-    }
-
+  protected isValidDelimiter(delimiter: string): boolean {
+    return delimiter.length === 1;
+  }
 }
